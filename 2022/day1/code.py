83dbc98d660098e1dc46186dfcd7a8e15d250963
kotakya <jack1108.ipod@gmail.com>
2022-12-01 00:16:15 -0500

    day1

--- 2022/day1/code.py
+++ 2022/day1/code.py
@@ -2,13 +2,32 @@ import itertools
 import collections
 import heapq
 
-with open("./2022/day1/input.txt") as data: data = [i.strip() for i in data.readlines()]
+with open("./2022/day01/input.txt") as data: data = [int(i.strip()) if i != "\n" else "" for i in data.readlines()]
 
 def part1():
-	pass
+	temp=0
+	array=[]
+	for i in data:
+		if i!="":
+			temp+=i
+		else:
+			array.append(temp)
+			temp=0
+	array.sort()
+	print(array[-1])
+
 
 def part2():
-	pass
+	temp=0
+	array=[]
+	for i in data:
+		if i!="":
+			temp+=i
+		else:
+			array.append(temp)
+			temp=0
+	array.sort()
+	print(array[-1]+array[-2]+array[-3])
 
 print('Part 1')
 part1()
